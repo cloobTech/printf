725f1f3fa07a48f1108f68b1b8542b3044376b58
JOSHUA <omojolajoshua@gmail.com>
2022-10-17 10:07:12 +0000

    print num func

--- main-2.c
+++ main-2.c
@@ -2,16 +2,22 @@
 
 
 /* Print number */
+int count_digits(int n);
 int _prntnum(int n)
 {
 	int k;
-	k = 0;
-
 	if (n > 9)
-		_prntnum(n / 10);
+	_prntnum(n / 10);
 	_putchar((n % 10) + '0');
-	k++;
-	return (k);
+	return (count_digits(n));
+}
+
+int count_digits (int n)
+{
+	int k;
+	if (n > 9)
+	return (1 + count_digits(n / 10));
+	return((sizeof(n % 10))/(sizeof(int)));
 }
 
 int main(void)
@@ -19,7 +25,7 @@ int main(void)
 
 int x;
 
-x = _prntnum(8878906);
+x = _prntnum(43906);
 _putchar('\n');
 _putchar(x + '0');
 _putchar('\n');
